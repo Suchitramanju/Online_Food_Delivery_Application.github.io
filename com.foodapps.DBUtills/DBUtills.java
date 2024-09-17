b0ad8fc054c53778e11a4eabb38fe22ead7d5bf1
Suchitramanju <140877849+Suchitramanju@users.noreply.github.com>
2024-09-17 10:51:35 +0530

    Add files via upload

---
+++ com.foodapps.DBUtills/DBUtills.java
@@ -0,0 +1,26 @@
+package com.foodapps.DBUtills;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+final public class DBUtills 
+{
+	private static Connection con=null;
+	private  static String url="jdbc:mysql://localhost:3306/foodapp";
+	private static String username="root";
+	private static String password="root";
+	public static Connection myconnect()
+	{
+		try 
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection(url,username,password);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+}
